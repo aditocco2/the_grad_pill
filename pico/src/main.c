725f42cdadcb54ba447ff23c0359a18e80e72f31
aditocco2 <aditocco2@gmail.com>
2026-04-28 18:40:06 -0400

    Added PCB holes to mounting sheet
    Also organized some of the files

--- pico/src/main.c
+++ pico/src/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "pico/stdlib.h"
 #include "pico/rand.h"
 #include "pico/time.h"
 
-#include "buttons.h"
 #include "sd_card.h"
 #include "hub75.h"
-
-#include "player.h"
+#include "hub75_text.h"
+#include "rgb565_colors.h"
 
 #define SWITCH_INTERVAL_INDEX 4
 #define USE_STATIC_MODE_INDEX 6
@@ -21,22 +21,56 @@
 
 #define MAX_MEDIA 146
 
+#define SECTORS_PER_FRAME (WIDTH * HEIGHT * 2 / BLOCK_SIZE)
+
+#define NUM_MEDIA_INDEX 0
+
+#define MEDIA_SECTOR_ADDR_INDEX 0
+#define N_FRAMES_INDEX 4
+#define FRAME_TIME_INDEX 8
+
+#define TABLE_ROW_WIDTH 16
+
 #define MODESEL 14
+#define STATIC_SWITCH_VALUE 0
+#define SLIDESHOW_SWITCH_VALUE 1
 
-_Bool mode_switch_flag;
-_Bool media_switch_flag;
+typedef enum {TEXT, IMAGE, VIDEO} media_type_t;
+typedef enum {STATIC, SLIDESHOW} mode_t;
 
-void top_level_fsm();
+typedef struct{
+    uint16_t switch_interval_s;
+    _Bool use_static_mode;
+    _Bool randomize;
 
-uint32_t get_new_media_index(uint32_t current_index, uint32_t num_media, 
-                            _Bool use_static_mode, _Bool randomize);
+    mode_t current_mode;
 
-void fill_array(uint32_t *arr, uint32_t len);
+    media_type_t current_media_type;
+    uint32_t media_address;
+    uint16_t frame_duration_ms;
+    uint32_t current_media_index;
+    uint32_t current_frame_num;
+    uint32_t num_frames_in_current_media;
+    uint32_t pool_size;
+} player_data_t;
+
+void main_fsm();
+
+void slideshow_media_index_fsm(player_data_t *ts, _Bool reset);
+_Bool player_get_metadata(player_data_t *ts, uint8_t *temp_buffer);
+_Bool player_load_media(player_data_t *ts, uint8_t *temp_buffer);
+_Bool player_get_frame(player_data_t *ts, uint8_t *frame_buffer);
+_Bool mode_switched(player_data_t *ts);
+
+void fill_array_sequentially(uint32_t *arr, uint32_t len);
 void shuffle_array(uint32_t *arr, uint32_t len);
 
-struct repeating_timer media_switch_timer;
-_Bool media_switch_cb(__unused repeating_timer_t *rt);
-void mode_switch_cb();
+// Basic timer callbacks that just set flags
+struct repeating_timer media_switch_timer, frame_switch_timer, main_loop_timer;
+_Bool media_switch_flag, frame_switch_flag, main_loop_flag;
+_Bool media_switch_cb(__unused repeating_timer_t *rt) {media_switch_flag = true;}
+_Bool frame_switch_cb(__unused repeating_timer_t *rt) {frame_switch_flag = true;}
+_Bool main_loop_cb(__unused repeating_timer_t *rt) {main_loop_flag = true;}
 
 int main(){
     stdio_init_all();
@@ -44,144 +78,223 @@ int main(){
     hub75_configure();
     hub75_set_brightness(20);
 
-    configure_button(MODESEL);
-    attach_press(MODESEL, mode_switch_cb);
+    gpio_init(MODESEL);
+    gpio_pull_up(MODESEL);
+    gpio_set_dir(MODESEL, GPIO_IN);
 
-    while(1){
-        sleep_ms(5);
-        top_level_fsm();
+    while(67){
+        main_fsm();
     }
 }
 
-void top_level_fsm(){
+void main_fsm(){
+    static enum {INIT, GET_METADATA, SWITCH_MODE, LOAD_MEDIA, LOAD_FRAME, WAIT} state;
 
-    static enum {INIT, SWITCH_MODE, GET_STATIC_MEDIA, SWITCH_MEDIA, WAIT} state;
-    static enum {STATIC_MODE, SLIDESHOW_MODE} mode;
+    static player_data_t ts;
+    
+    static _Bool sd_succ = true;
 
-    static uint32_t media_index;
-    static uint32_t num_media;
+    uint8_t temp_buffer[BLOCK_SIZE];
+    uint8_t *frame_buffer = (uint8_t *)hub75_get_back_buffer();
 
-    static uint16_t switch_interval_s;
-    static _Bool use_static_mode;
-    static _Bool randomize;
-
-    _Bool success;
-
-    static uint8_t table_header[TABLE_ROW_WIDTH];
+    if(!sd_succ){
+        for(uint8_t x = 0; x < WIDTH; x++){
+            for(uint8_t y = 0; y < HEIGHT; y++){
+                hub75_set_pixel(x, y, 0);
+            }
+        }
+        hub75_write_large_text("Insert", 32, 32, ALIGN_CENTER, ALIGN_BOTTOM, RGB565_Red);
+        hub75_write_large_text("SD Card", 32, 32, ALIGN_CENTER, ALIGN_TOP, RGB565_Red);
+        hub75_update();
+        state = INIT;
+    }
 
     switch(state){
         case INIT:
-            cancel_repeating_timer(&media_switch_timer);
-            success = player_init();
-            if(success){    
-                success = sd_card_read_block(0, table_header, TABLE_ROW_WIDTH);
-            }
-            if(success){
-                num_media = player_get_num_media();
-                media_index = -1; // so any media can be picked at first
-                switch_interval_s = *(uint16_t *)&table_header[SWITCH_INTERVAL_INDEX];
-                use_static_mode = *(_Bool *)&table_header[USE_STATIC_MODE_INDEX];
-                randomize = *(_Bool *)&table_header[RANDOMIZE_INDEX];
+            sd_succ = sd_card_init();
+            state = GET_METADATA;
+            break;
 
-                if(use_static_mode){
-                    mode = STATIC_MODE;
-                    state = GET_STATIC_MEDIA;
-                }
-                else{
-                    mode = SLIDESHOW_MODE;
-                    state = SWITCH_MEDIA;
-                }
-            }
+        case GET_METADATA:
+            sd_succ = player_get_metadata(&ts, temp_buffer);
+            slideshow_media_index_fsm(&ts, true);
+            state = SWITCH_MODE;
             break;
 
         case SWITCH_MODE:
-            if(mode == STATIC_MODE){
-                mode = SLIDESHOW_MODE;
-                state = SWITCH_MEDIA;
-                add_repeating_timer_ms(-1000 * switch_interval_s,
-                                       media_switch_cb, NULL, &media_switch_timer);
-            }
-            else if(mode == SLIDESHOW_MODE && use_static_mode){
-                mode = STATIC_MODE;
-                state = GET_STATIC_MEDIA;
-                cancel_repeating_timer(&media_switch_timer);
+            cancel_repeating_timer(&media_switch_timer);
+            if(gpio_get(MODESEL) == STATIC_SWITCH_VALUE && ts.use_static_mode){
+                ts.current_mode = STATIC;
             }
             else{
-                state = SWITCH_MEDIA;
+                ts.current_mode = SLIDESHOW;
+                add_repeating_timer_ms(ts.switch_interval_s * 1000, media_switch_cb, NULL, 
+                                       &media_switch_timer);
             }
-            break;  
-
-        case GET_STATIC_MEDIA:
-            media_index = num_media - 1; // static mode uses last image on the card
-            success = player_load_media(media_index);
-            if(success){
-                state = WAIT;
-            }
-            else{
-                state = INIT;
-            }
+            state = LOAD_MEDIA;
             break;
 
-        case SWITCH_MEDIA:
-            media_index = get_new_media_index(media_index, num_media, use_static_mode, randomize);
-            success = player_load_media(media_index);
-            if(success){
-                state = WAIT;
+        case LOAD_MEDIA:
+            if(ts.current_mode == STATIC){
+                ts.current_media_index = ts.pool_size;
             }
             else{
-                state = INIT;
+                slideshow_media_index_fsm(&ts, false);
             }
+            sd_succ = player_load_media(&ts, temp_buffer);
+            state = LOAD_FRAME;
+            break;
+        
+        case LOAD_FRAME:
+            sd_succ = player_get_frame(&ts, frame_buffer);
+            hub75_update();
+            state = WAIT;
             break;
 
         case WAIT:
-            success = player_update();
-            success = sd_card_check_status();
             if(media_switch_flag){
                 media_switch_flag = false;
-                state = SWITCH_MEDIA;
+                state = LOAD_MEDIA;
             }
-            if(mode_switch_flag){
-                mode_switch_flag = false;
+            else if(frame_switch_flag){
+                frame_switch_flag = false;
+                state = LOAD_FRAME;
+            }
+            else if(mode_switched(&ts)){
                 state = SWITCH_MODE;
             }
-            if(!success){
-                state = INIT;
+            else{
+                sd_succ = sd_card_check_status();
             }
             break;
-    }       
+    }
 }
 
-uint32_t get_new_media_index(uint32_t current_index, uint32_t num_media, 
-                            _Bool use_static_mode, _Bool randomize){
+_Bool player_get_metadata(player_data_t *ts, uint8_t *temp_buffer){
 
-    uint32_t media_index;
-
-    uint32_t num_slideshow_media = num_media; 
-    if(use_static_mode){
-        num_slideshow_media--;
+    if(!sd_card_read_block(0, temp_buffer, BLOCK_SIZE)){
+        return false;
     }
 
-    if(!randomize){
-        media_index = (current_index + 1) % num_slideshow_media;
+    ts->switch_interval_s = *(uint16_t *)&temp_buffer[SWITCH_INTERVAL_INDEX];
+    ts->use_static_mode = *(_Bool *)&temp_buffer[USE_STATIC_MODE_INDEX];
+    ts->randomize = *(_Bool *)&temp_buffer[RANDOMIZE_INDEX];
+
+    ts->pool_size = *(uint32_t *)&temp_buffer[NUM_MEDIA_INDEX];
+    if(ts->use_static_mode){
+        ts->pool_size = ts->pool_size - 1;
     }
-    else if(num_slideshow_media == 1){
-        media_index = 0;
+
+    return true;
+}
+
+void slideshow_media_index_fsm(player_data_t *ts, _Bool reset){
+    static enum {INIT, SHUFFLE, TRAVERSE} state;
+
+    static uint32_t media_index_array[MAX_MEDIA];
+    static uint32_t tag = 0;
+
+    if(reset){
+        state = INIT;
+    }
+
+    switch(state){
+        case INIT:
+            fill_array_sequentially(media_index_array, ts->pool_size);
+            tag = 0;
+            if(ts->randomize){
+                state = SHUFFLE;
+            }
+            else{
+                state = TRAVERSE;
+            }
+            break;
+        case SHUFFLE:
+            shuffle_array(media_index_array, ts->pool_size);
+            tag = 0;
+            state = TRAVERSE;
+            break;
+        case TRAVERSE:
+            tag++;
+            if(ts->randomize && tag >= ts->pool_size - 1){
+                state = SHUFFLE;
+            }
+            else if(tag >= ts->pool_size){
+                tag = 0;
+            }
+            break;
+    }
+
+    ts->current_media_index = media_index_array[tag];
+
+    // printf("[ ");
+    // for(uint32_t i = 0; i < ts->pool_size; i++){
+    //     printf("%d ", media_index_array[i]);
+    // }
+    // printf("] -> %d\n", ts->current_media_index);
+}
+
+_Bool player_load_media(player_data_t *ts, uint8_t *temp_buffer){
+    
+    cancel_repeating_timer(&frame_switch_timer);
+
+    uint32_t table_row = ts->current_media_index + 1;
+    uint16_t sector = table_row / (BLOCK_SIZE / TABLE_ROW_WIDTH);
+    uint16_t table_row_index = table_row * TABLE_ROW_WIDTH % BLOCK_SIZE;
+
+    if(!sd_card_read_block(sector, temp_buffer, BLOCK_SIZE)){
+        return false;
+    }
+
+    ts->media_address = *(uint32_t *)&temp_buffer[table_row_index + MEDIA_SECTOR_ADDR_INDEX];
+    ts->num_frames_in_current_media = *(uint32_t *)&temp_buffer[table_row_index + N_FRAMES_INDEX];
+    ts->frame_duration_ms = *(uint16_t *)&temp_buffer[table_row_index + FRAME_TIME_INDEX];
+
+    ts->current_frame_num = 0;
+
+    // maybe replace with a new flag on the SD row, idk.
+    // the current_media_type is not currently used, but it will be if I do scrolling text
+    if(ts->num_frames_in_current_media > 1){
+        ts->current_media_type = VIDEO;
+        add_repeating_timer_ms(ts->frame_duration_ms, frame_switch_cb, NULL, &frame_switch_timer);
     }
     else{
-        // get a new number that's not the previous one
-        uint32_t random_number = (get_rand_32() >> 8 & 0xFFFF) % (num_slideshow_media - 1);
-        if(random_number >= current_index){
-            media_index = random_number + 1;
-        }
-        else{
-            media_index = random_number;
-        }   
+        ts->current_media_type = IMAGE;
     }
 
-    return media_index;
+    return true;
 }
 
-void fill_array(uint32_t *arr, uint32_t len){
+_Bool player_get_frame(player_data_t *ts, uint8_t *frame_buffer){
+    if(!sd_card_read_blocks(ts->media_address + (ts->current_frame_num * SECTORS_PER_FRAME), 
+                            SECTORS_PER_FRAME, frame_buffer)){
+        return false;
+    }
+
+    ts->current_frame_num = ts->current_frame_num + 1;
+    if(ts->current_frame_num == ts->num_frames_in_current_media){
+        ts->current_frame_num = 0;
+    }
+
+    return true;
+}
+
+_Bool mode_switched(player_data_t *ts){
+    if(ts->use_static_mode == false){
+        return false;
+    }
+    else if(ts->current_mode == STATIC && gpio_get(MODESEL) == SLIDESHOW_SWITCH_VALUE){
+        return true;
+    }
+    else if(ts->current_mode == SLIDESHOW && gpio_get(MODESEL) == STATIC_SWITCH_VALUE){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+void fill_array_sequentially(uint32_t *arr, uint32_t len){
     for(uint32_t i = 0; i < len; i++){
         arr[i] = i;
     }
@@ -195,13 +308,4 @@ void shuffle_array(uint32_t *arr, uint32_t len){
         arr[j] = arr[i];
         arr[i] = temp;
     }
-}
-
-_Bool media_switch_cb(__unused repeating_timer_t *rt){
-    media_switch_flag = true;
-    return true;
-}
-
-void mode_switch_cb(){
-    mode_switch_flag = true;
 }
---
+++ python/main.py
@@ -0,0 +1,182 @@
+import os
+import math
+import shutil
+import warnings
+from image_proc import *
+
+# ------------------- User Parameters ---------------------
+
+media_dir = "media_slideshow"
+sd_file = "raw/media.bin"
+
+width, height = 64, 64
+switch_interval = 10
+stretch = True
+randomize = True
+
+use_static_mode = True
+static_mode_image = "media_static/happy.gif"
+
+# ---------------------------------------------------------
+
+def main():
+    global use_static_mode
+
+    warnings.filterwarnings("ignore")
+
+    media_files = [f"{media_dir}/{f}" for f in os.listdir(media_dir)]
+    file_count = len(media_files) + (1 if use_static_mode else 0)
+
+    data_file = open("temp_data.bin", "wb")
+
+    # Keep all media info, as we need to dynamically change sector addresses later
+    media_info_list = []
+
+    # Keep track of how many files were successfully processed
+    num_media = 0
+
+    RED = '\033[91m'
+    RESET = '\033[0m'
+    # Slideshow Mode
+    for i, file in enumerate(media_files):
+        try:
+            media_info, data_array = process_media(file)
+            media_info_list.append(media_info)
+            data_file.write(data_array)
+            num_media += 1
+            print(f"Processed {file} ({i+1}/{file_count})")
+
+        except ProcessingFailedError:
+            print(f"{RED}Failed to process {file} ({i+1}/{file_count}){RESET}")
+        except FileNotSupportedError:
+            print(f"{RED}File {file} not supported ({i+1}/{file_count}){RESET}")
+    
+    # Static Mode
+    if(use_static_mode):
+        try:
+            media_info, data_array = process_media(static_mode_image)
+            media_info_list.append(media_info)
+            data_file.write(data_array)
+            num_media += 1
+            print(f"Processed {static_mode_image} for static mode ({file_count}/{file_count})")
+
+        except ProcessingFailedError:
+            print(f"{RED}Failed to process {file} ({file_count}/{file_count}). "
+                  + f"Disabling static mode{RESET}")
+            use_static_mode = False
+        except FileNotSupportedError:
+            print(f"{RED}File {file} not supported ({file_count}/{file_count}). "
+                  + f"Disabling static mode{RESET}")
+            use_static_mode = False
+    
+    data_file.close()
+    # We are finished writing pixel data at this point. Now for the table of contents
+
+    # 16-byte row for each media, plus a 16-byte header
+    table_sectors = math.ceil((num_media + 1) * 16 / 512)
+
+    table = bytearray(table_sectors * 512)
+
+    make_table_header(table, num_media)
+    make_table_rows(table, table_sectors, media_info_list)
+
+    table_file = open("temp_table.bin", "wb")
+    table_file.write(table)
+    table_file.close()
+
+    # And put 'em together
+    file_concat(["temp_table.bin", "temp_data.bin"], sd_file, clean=True)
+
+    print("Done!")
+    print(f"Your file is ready in {sd_file}")
+
+def make_table_header(table, num_media):
+    # Table Header Format:
+    # bytes 0 to 3 are the number of pictures/videos
+    # bytes 4 to 5 are how many seconds between switching
+    # byte 6 is whether to include static mode
+    # byte 7 is whether to randomize playback
+    table_row = bytearray(16)
+    table_row[0:4] = to_little_endian(num_media, 4)
+    table_row[4:6] = to_little_endian(int(switch_interval), 2)
+    table_row[6] = 1 if use_static_mode else 0
+    table_row[7] = 1 if randomize else 0
+    table[0:16] = table_row
+
+
+def make_table_rows(table, table_sectors, media_info_list):
+    # Table Row Format:
+    # bytes 0 to 3 are the sector address of the first frame of the video
+    # bytes 4 to 7 are the number of frames in the video
+    # bytes 8 to 9 are how long each frame lasts in milliseconds
+    sector_num = table_sectors
+    sectors_per_frame = math.ceil(width * height * 2 / 512)
+    for index, info in enumerate(media_info_list):
+        table_row = bytearray(16)
+        table_row[0:4] = to_little_endian(sector_num, 4)
+        table_row[4:8] = to_little_endian(info["n_frames"], 4)
+        table_row[8:10] = to_little_endian(info["frame_time_ms"], 2)
+
+        table[(index + 1) * 16 : (index + 2) * 16] = table_row
+        sector_num += (info["n_frames"] * sectors_per_frame)
+
+
+def process_media(file):
+
+    image_types = [".png", ".jpg", ".jpeg"]
+    video_types = [".mp4", ".mov", ".avi", ".mkv"]
+    
+    extension = os.path.splitext(file)[1].lower()
+    if extension in image_types:
+        try:
+            data_array = image_to_bytearray(file, width, height, stretch)
+            media_info = {
+                "frame_time_ms": 0,
+                "n_frames": 1
+            }
+        except:
+            raise ProcessingFailedError
+    elif extension in video_types:
+        try:
+            data_array = video_to_bytearray(file, width, height, stretch)
+            media_info = {
+                "frame_time_ms": get_video_frame_time_ms(file),
+                "n_frames": get_video_n_frames(file)
+            }
+        except:
+            raise ProcessingFailedError
+    elif extension == ".gif":
+        try:
+            data_array = gif_to_bytearray(file, width, height, stretch)
+            media_info = {
+                "frame_time_ms": get_gif_frame_time_ms(file),
+                "n_frames": get_gif_n_frames(file)
+            }
+        except:
+            raise ProcessingFailedError
+    else: # file not supported
+        raise FileNotSupportedError
+
+    return media_info, data_array
+
+
+def file_concat(files_in, file_out, clean=False):
+    with open(file_out, "wb") as outfile:
+        for fname in files_in:
+            with open(fname, "rb") as infile:
+                shutil.copyfileobj(infile, outfile)
+                    
+    if clean:
+        for f in files_in:
+            os.remove(f)
+
+
+def to_little_endian(number, bytes):
+    ba = bytearray(bytes)
+    for i in range(bytes):
+        ba[i] = number >> (8 * i) & 0xFF
+    return ba
+
+
+if __name__ == "__main__":
+    main()
